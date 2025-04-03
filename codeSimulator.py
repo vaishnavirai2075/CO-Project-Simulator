878203b369ec581d290b42ad43fc305101ff1208
vaishnavirai2075 <vaishnavi24599@iiitd.ac.in>
2025-04-03 20:02:58 +0530

    file handling

--- codeSimulator.py
+++ codeSimulator.py
@@ -190,3 +190,93 @@ def rvrs(instruction):
 
 def halt():
     return True
+
+program_counter = 0                
+instr_dict = {}
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+with open(input_file, "r") as f:
+    lines = f.readlines()
+    for i in range(len(lines)):
+        instr_dict[i] = lines[i].strip()
+
+virtual_halt = "00000000000000000000000001100011"
+
+with open(output_file, "w") as file:
+    while instr_dict[program_counter] != virtual_halt:
+        instruction = instr_dict[program_counter]
+        opcode = instruction[25:32]
+
+        if opcode in R_type:
+            funct7 = instruction[:7]
+            funct3 = instruction[-15:-12]
+            if funct7 == "0100000":
+                 sub(instruction)
+                 program_counter = program_counter+1
+            else:
+                if funct3 == "000":
+                    add(instruction)
+                    program_counter = program_counter+1
+                elif funct3 == "010":
+                    slt(instruction)
+                    program_counter = program_counter+1
+                elif funct3 == "101":
+                    srl(instruction)
+                    program_counter = program_counter+1
+                elif funct3 == "110":
+                    or_(instruction)
+                    program_counter = program_counter+1
+                elif funct3 == "111":
+                    and_(instruction)
+                    program_counter = program_counter+1
+
+        elif opcode in I_type:
+            if opcode == "0000011":
+                lw(instruction)
+                program_counter = program_counter+1
+            elif opcode == "1100111":
+                program_counter = jalr(instruction, program_counter)
+            elif opcode == "0010011":
+                funct3 = instruction[-15:-12]
+                if funct3 == "000":
+                    addi(instruction)
+                    program_counter = program_counter+1
+
+        elif opcode in S_type:
+            sw(instruction)
+            program_counter = program_counter+1
+
+        elif opcode in B_type:
+            funct3 = instruction[-15:-12]
+            if funct3 == "000":
+                program_counter = beq(instruction, program_counter)
+            elif funct3 == "001":
+                program_counter = bne(instruction, program_counter)
+            elif funct3 == "100":
+                program_counter = blt(instruction, program_counter)
+
+        elif opcode in J_type:
+            program_counter = jal(instruction, program_counter)
+            
+        elif opcode in BONUS_type:
+            funct3 = instruction[-15:-12]
+            if funct3 == "000":  
+                mul(instruction)
+                program_counter = program_counter+1
+            elif funct3 == "001":  
+                rst()
+                program_counter = program_counter+1
+            elif funct3 == "010":  
+                program_counter = len(instr_dict)
+                break
+            elif funct3 == "011":  
+                rvrs(instruction)
+                program_counter = program_counter+1
+
+        else:
+            program_counter += 1
+        
+        file.write(f"0b{bin(program_counter*4 & 0xffffffff)[2:].zfill(32)} ")
+        for reg in registers:
+            file.write(f"0b{bin(registers[reg] & 0xffffffff)[2:].zfill(32)} ")
+        file.write("\n")
